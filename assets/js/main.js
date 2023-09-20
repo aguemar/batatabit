811246e05cc5fad9b01ccc39c4ca3df76320353a
Alejandro Guevara Martinez <aguemar.work@gmail.com>
2023-09-20 17:17:50 -0500

    V3

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,72 @@
-const table1 = document.getElementById("table1");
-const table2 = document.getElementById("table2");
+// const prevButton = document.getElementById("prevButton");
+// const nextButton = document.getElementById("nextButton");
+// const tabla1 = document.getElementById("table1");
+// const tabla2 = document.getElementById("table2");
+
+// let currentTabla = 1;
+// let mobile = window.innerWidth < 768;
+
+// tabla1.style.display = "block";
+
+// nextButton.addEventListener("click", () => {
+//   if (currentTabla === 1 && mobile) {
+//     tabla1.style.display = "none";
+//     tabla2.style.display = "block";
+//     prevButton.disabled = false;
+//     nextButton.disabled = true;
+//     currentTabla = 2;
+//   }
+// });
+
+// prevButton.addEventListener("click", () => {
+//   if (currentTabla === 2 && mobile) {
+//     tabla2.style.display = "none";
+//     tabla1.style.display = "block";
+//     prevButton.disabled = true;
+//     nextButton.disabled = false;
+//     currentTabla = 1;
+//   }
+// });
+
 const prevButton = document.getElementById("prevButton");
 const nextButton = document.getElementById("nextButton");
+const tabla1 = document.getElementById("table1");
+const tabla2 = document.getElementById("table2");
 
-let activeTable = table1;
+let currentTabla = 1;
+let mobile = window.innerWidth < 768;
 
-prevButton.addEventListener("click", () => {
-  if (activeTable === table2) {
-    table2.classList.remove("active");
-    table1.classList.add("active");
-    nextButton.classList.remove("disabled");
-    prevButton.classList.add("disabled");
-    activeTable = table1;
+tabla1.style.display = "block";
+
+function mostrarTabla(numeroTabla) {
+  if (numeroTabla === 1) {
+    tabla1.style.display = "block";
+    tabla2.style.display = "none";
+    prevButton.disabled = true;
+    nextButton.disabled = false;
+  } else {
+    tabla1.style.display = "none";
+    tabla2.style.display = "block";
+    prevButton.disabled = false;
+    nextButton.disabled = true;
   }
-});
+  currentTabla = numeroTabla;
+}
 
 nextButton.addEventListener("click", () => {
-  if (activeTable === table1) {
-    table1.classList.remove("active");
-    table2.classList.add("active");
-    prevButton.classList.remove("disabled");
-    nextButton.classList.add("disabled");
-    activeTable = table2;
+  if (currentTabla === 1 && mobile) {
+    mostrarTabla(2);
   }
 });
+
+prevButton.addEventListener("click", () => {
+  if (currentTabla === 2 && mobile) {
+    mostrarTabla(1);
+  }
+});
+
+// Para manejar el caso de escritorio
+if (!mobile) {
+  prevButton.style.display = "none";
+  nextButton.style.display = "none";
+}
